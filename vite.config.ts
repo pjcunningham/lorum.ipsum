5ca9d415ed252467897f62bdc0ae4cf797464ed8
Paul Cunningham <pjcunningham@borsuk.co.uk>
2025-10-29 22:58:54 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    // Use repo name for project pages. If you later use a custom domain, you can switch this to '/'.
     base: mode === 'production' ? '/lorum.ipsum/' : '/',
 }))
